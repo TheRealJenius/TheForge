061c0bde8658c58335bf7f89663625a1bd0396ff
Jesse Appiah <TheRealJenius@gmail.com>
2020-02-19 22:07:48 +0000

    While and for statement

--- Python/Welcome.py
+++ Python/Welcome.py
@@ -42,3 +42,23 @@ letters[2:4] =[] #clearing list for position 2,3
 print (letters)
 
 
+x = int ( input (" Please Enter a number/integer: "))
+if x <0:
+    x = 0
+    print ("Negative changed to '0'")
+elif x == 0: #elif needs to be in the same indentation as if statement
+    print ('Zero')
+elif x == 1:
+    print ('One')
+else: #else statment doesn't need to be defined, as it will be the case if all-else fails, no pun intended
+    print ('More than one')
+
+
+a,b = 0,1
+while a<100: #executes as long as the condition remains true 
+    print (a, end=',') #end avoids a new line
+    a,b = b, a+b
+
+words = ['wolf', 'panda', 'whale']
+for w in words:
+    print (w, len(w))
