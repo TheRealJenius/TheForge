34d7a0763886b600c39e546d500c630b3d41e600
Jesse Appiah <TheRealJenius@gmail.com>
2020-04-30 19:47:48 +0100

    Folder now has its own repo HackerRank

--- Python/HackerRank/add subtract multiply divide.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a+b) # addition
-    print(a-b) # subtraction
-    print(a*b) # multiplication
-    print(a//b) # integer division
-    print(a/b) # float division
